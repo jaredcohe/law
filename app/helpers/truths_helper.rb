92e19204c905ad358f433f2cdbc8ac982bb4c0d4
Jared Cohen <jec1477@gmail.com>
2011-02-25 18:28:19 -0800

    started work on disqus

--- app/helpers/truths_helper.rb
+++ app/helpers/truths_helper.rb
@@ -1,2 +1,20 @@
 module TruthsHelper
+  def disqus_init_js
+    js =<<EOS
+      /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+      var disqus_shortname = 'thetruthaboutlawschool'; // required: replace example with your forum shortname
+
+      // The following are highly recommended additional parameters. Remove the slashes in front to use.
+      var disqus_identifier = #{@truth.id};
+      var disqus_url = 'http://thetruthaboutlawschool.com/permalink-to-page.html';
+
+      /* * * DON'T EDIT BELOW THIS LINE * * */
+      (function() {
+          var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+          dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+          (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+      })();
+EOS
+    javascript_tag js
+  end
 end
