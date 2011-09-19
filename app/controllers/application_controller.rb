3637148f7f7f4a59e71d6ce97e788299178fe134
jec1477 <jec1477@gmail.com>
2011-09-19 16:14:40 -0400

    stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate
     authenticate_or_request_with_http_basic do |user_name, password|
-      user_name == 'kagan' && password == 'l_wetsuit'
+      user_name == 'kagan' && password == 'hidden'
     end
   end
 end
