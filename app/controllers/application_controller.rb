888d75a00227aacdc92f26e52fb85a4f0c431027
jec1477 <jec1477@gmail.com>
2011-09-19 16:20:40 -0400

    p

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate
     authenticate_or_request_with_http_basic do |user_name, password|
-      user_name == 'kagan' && password == 'hidden'
+      user_name == 'kagan' && password == 'l_merkin'
     end
   end
 end
